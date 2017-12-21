3dbdb170187323cb36794f3fc4fad9add4c19b1b
meganparadowski <34354237+meganparadowski@users.noreply.github.com>
2017-12-20 18:28:32 -0700

    Update scrub.py

--- scrub.py
+++ scrub.py
@@ -1,20 +1,3 @@
-"""
-Link to data source: https://data.cityofnewyork.us/Education/School-Safety-Report/qybk-bjjc/data
-Records I am working with: first 1100
-
-Accomplishments of this program:
-1. change N/A's to blanks
-2. remove all calculated averages
-3. change the "Schools in Building" column from the names separated by '|' to
-   a python array so that it could be easier manipulated later
-4. remove schools with a blank "Register" column (schools with no registered students)
-5. remove building name from "Schools in Building" list where it was added unnecessarily
-   to many records
-NOTE: schools with blank or unavailable crime numbers I have left as blank (I do
-not want to simply assume there were 0 crimes - this is something that would need
-fact-checking)
-"""
-
 #import csv module to read and write with csv reader and writer
 import csv
 
